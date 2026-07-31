15ba265160c1f52c36644fbb38b9a6ec7d2a7702
Omar Abdiwali <omarabdiwali17@gmail.com>
2026-07-30 19:43:14 -0600

    added analytics and seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Header } from "@/components/Header";
 import { SnackbarProvider } from "notistack";
+import { Analytics } from "@vercel/analytics/react"
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,6 +17,10 @@ export default function App({ Component, pageProps }: AppProps) {
         </Head>
         <Header />
         <Component {...restPageProps} />
+        <Analytics beforeSend={(event) => {
+          if (typeof window != 'undefined' && localStorage.getItem('skipAnalytics') == 'true') return null;
+          return event;
+        }} />
       </SessionProvider>
     </SnackbarProvider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="FT-zhFm6Eyxo29YZJaAICTigJzNgDzoWVue_2yv7cSs" />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body className="antialiased">
